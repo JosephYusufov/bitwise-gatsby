dabdd43e9edf66c6d7ddd335b4eccfe3bebb9286
Joseph Yusufov <jyusufov00@stuy.edu>
2020-06-13 10:24:50 -0400

    im fried, will come back to this later

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,22 @@
  */
 
 // You can delete this file if you're not using it
+import { graphql, useStaticQuery } from 'gatsby';
+
+exports.createPages = ({ actions }) => {
+    const { createPage } = actions;
+    const { data } = useStaticQuery(graphql`
+        query files {
+            allFile(filter: {extension: {eq: "json"}}) {
+                edges {
+                    node {
+                        id
+                        name
+                        extension
+                    }
+                }    
+            }
+        }
+    `);
+    console.log(data);
+};
